f7cf15a49a26269be44502271f9bd1bde83a4bec
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2025-07-05 17:54:24 +0200

    layout drafted

--- libs/ui-layout/src/lib/layout/layout.component.ts
+++ libs/ui-layout/src/lib/layout/layout.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
     selector: 'lib-ui-layout',
-    imports: [CommonModule, HeaderComponent, RouterModule],
+    imports: [
+        RouterModule,
+        CommonModule,
+        HeaderComponent,
+        FooterComponent
+    ],
     templateUrl: './layout.component.html',
     styleUrl: './layout.component.scss',
 })
