227ad560d0ebc22273a9d2a975ae3ad0735db042
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2025-07-11 20:32:06 +0200

    server setup to return sample data, tweaked for mydevil dedployments

--- apps/angular-tribe-api/src/app.ts
+++ apps/angular-tribe-api/src/app.ts
@@ -1,14 +1,28 @@
 import express from 'express';
-
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+import { readFile } from 'fs/promises';
 
 const app = express();
+
+app.get('/sample', async (req, res) => {
+    try {
+        const filePath = './public/test-article.md';
+        const content = await readFile(filePath, 'utf-8');
+
+        res.json({
+            title: 'UI component library',
+            size: content.length,
+            content,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to read file' });
+    }
+});
 
 app.get('/', (req, res) => {
     res.send({ message: 'Hello API' });
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+app.listen(() => {
+    console.log(`[ ready ] application started`);
 });
