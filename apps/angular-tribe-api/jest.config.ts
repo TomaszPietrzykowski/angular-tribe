5f30ec7b5b01be07acdf2adf96345d059aa0fc69
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2025-07-11 19:58:07 +0200

    api project added to workspace

---
+++ apps/angular-tribe-api/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'angular-tribe-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/angular-tribe-api',
+};
